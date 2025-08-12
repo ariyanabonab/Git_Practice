bb913a63130f3d1128192d46ad5e76ee7705a628
Ariyana Bonab <138933891+ariyanabonab@users.noreply.github.com>
2025-08-12 11:54:02 -0400

    functions.py

--- functions.py
+++ functions.py
@@ -10,21 +10,20 @@ def print_the_number_of_this_room():
 
 def split_this_string_at_each_space(input_string):
     s = "milkshake"
-    print(s.split("")
+    print(s.split(""))
 
 def take_the_average_of_these_numbers(a, b):
-    a = 5
-    b = 7
-    print(np.mean(a+b))
+    print(np.mean([a, b]))
 
 def print_the_time_now_using_astropy():
-    raise NotImplementedError
+    from astropy.time import Time
 
 def return_the_minimum_of_two_numbers(a, b):
-    raise NotImplementedError
+    a = 5
+    b = 7
+    print(np.min[a,b])
 
 def return_the_std_of_two_numbers(a, b):
-    a = 12
-    b = 24
-    print(np.std(a,b))
-
+    a = 5
+    b = 7
+    print(np.std([a,b]))
